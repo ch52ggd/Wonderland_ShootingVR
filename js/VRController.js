afc13ce199a56e54bb1da3a2b008d28ba1f0a227
ch52ggd <20200887@gm.hannam.ac.kr>
2023-10-18 13:40:45 +0900

    select & squeeze
    select & squeeze

--- js/VRController.js
+++ js/VRController.js
@@ -16,28 +16,62 @@ export class VRController extends Component {
          * that your component may create. */
     }
 
+    LHand;
+    RHand;
+
     init() {
-        console.log('init() with param', this.param);
+        //console.log('init() with param', this.param);
+
+        this.inputListener();
     }
 
     start() {
-        console.log('start() with param', this.param);
-
-        this.engine.xr.session.addEventListener("selectstart", this.selectstart.bind(this));
-        
-
-        if(this.checking === true){
-
-            console.log("???");
-        }
+        //console.log('start() with param', this.param);
     }
 
     update(dt) {
         /* Called every frame. */
     }
 
+
+
+    inputListener(){
+
+        this.engine.onXRSessionStart.add((session, mode) => {
+
+            console.log("XR Session Start");
+            
+            //select
+            this.engine.xr.session.addEventListener("selectstart", this.selectstart.bind(this));
+            this.engine.xr.session.addEventListener("selectend", this.selectend.bind(this));
+            
+
+            //squeeze
+            this.engine.xr.session.addEventListener("squeezestart", this.squeezestart.bind(this));
+            this.engine.xr.session.addEventListener("squeezeend", this.squeezeend.bind(this));
+        })
+    }
+
+
+
+    //select
     selectstart(e){
 
-        this.checking = true;
+        console.log("select start event");
     }
+    selectend(e){
+
+        console.log("select end event");
+    }
+
+    //squeeze
+    squeezestart(e){
+
+        console.log("squeeze start event");
+    }
+    squeezeend(e){
+
+        console.log("squeeze end event");
+    }
+    
 }
